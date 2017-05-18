ad62afb961212887a5cd66d14557c5c668bf6776
Front Developer <frontdeveloper@Fronts-iMac.local>
2017-05-18 14:55:59 -0500

    Corrige la url de las pruebas e2e

--- e2e/helpers/variables.js
+++ e2e/helpers/variables.js
@@ -5,7 +5,7 @@ module.exports = {
   MAX_SAFE_TIMEOUT: Math.pow(2, 31) - 1,
   SLEEP_TIME      : 1500,
   HOME_STATE      : 'dashboard',
-  ADMIN_PATH      : 'http://localhost:3000/#!/',
+  ADMIN_PATH      : 'http://localhost:3000/',
   DASHBOARD_STATE : 'detalle-servicio'
 
 };
