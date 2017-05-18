21c225ac750adaccea19cb8cc3272ff04e67851a
Front Developer <frontdeveloper@Fronts-iMac.local>
2017-05-18 10:26:13 -0500

    Mueve la prueba e2e de login a su propio it()

--- e2e/specs/loginSpec.js
+++ e2e/specs/loginSpec.js
@@ -1,17 +1,17 @@
 var loginPage = require('../pages/loginPage.js');
 var variables = require('../helpers/variables.js');
 
-describe('Login to the application', function() {
+describe('Login to the application', function () {
 
-  it('Should navigate to login page', function(){
-      loginPage.go();
+  it('Should navigate to login page', function () {
+    loginPage.go();
   }, variables.MAX_SAFE_TIMEOUT);
 
-  it('Should prevent form submission if form is dirty', function(){
+  it('Should prevent form submission if form is dirty', function () {
     loginPage.email.sendKeys('aasdlasdfasdffkjasdf');
     // Password is required and email is invalid, it should prevent form submission
     expect(loginPage.loginBtn.getAttribute('disabled')).toEqual('true');
-    
+
     loginPage.resetForm();
     loginPage.email.sendKeys('s.polanco@mensajerosurbanos.com');
     loginPage.password.sendKeys('aasdlasdfasdffkjasdf');
@@ -22,7 +22,9 @@ describe('Login to the application', function() {
     // 1 second sleep to wait the toast to render
     browser.sleep(variables.SLEEP_TIME);
     expect(loginPage.errorToast.getText()).toEqual(loginPage.errorToastText);
+  });
 
+  it('Should login an user', function () {
     loginPage.resetForm();
     loginPage.email.sendKeys(loginPage.userEmail);
     loginPage.password.sendKeys(loginPage.userPass);
