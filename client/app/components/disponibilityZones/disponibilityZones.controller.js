5a47cb31c2d7e43fa38232220b5fe18cfeb530e5
Front Developer <frontdeveloper@Fronts-iMac.local>
2017-05-22 17:21:33 -0500

    Implementa el modal para ver las zonas en la vista de disponibilidades

--- client/app/components/disponibilityZones/disponibilityZones.controller.js
+++ client/app/components/disponibilityZones/disponibilityZones.controller.js
@@ -2,7 +2,6 @@
  * @author Juan Sebastian Polanco Ramos <s.polanco@mensajerosurbanos.com>
  */
 import viewZoneModal from './components/viewZoneModal/viewZoneModal.html';
-import { viewZoneModalController as viewZoneModalController } from './components/viewZoneModal/viewZoneModal.controller';
 // Class representing a DisponibilitySpots element
 
 class DisponibilityZonesController {
@@ -39,32 +38,85 @@ class DisponibilityZonesController {
     this.mdDialog.hide();
   }
 
-  testingModals(ev, template, controller) {
+  testingModals(ev, template) {
+
+  }
+
+  openViewZoneModal(ev, polygons){
     const self = this;
     this.mdDialog.show({
-      controller: controller,
+      controller: function($timeout){
+      console.log(polygons);
+      let vm = this;
+      let points = polygons.split(',');
+      let pointsArray = [];
+      for (var i in points) {
+        let latLon = points[i].split(' ');
+        pointsArray.push([latLon[0], latLon[1]]);
+      }
+      console.log(pointsArray);
+
+      let polygon = new ol.geom.Polygon([pointsArray]);
+      polygon.transform('EPSG:4326', 'EPSG:3857');
+
+      const feature = new ol.Feature(polygon);
+
+      // Create vector source and the feature to it.
+      const vectorSource = new ol.source.Vector();
+      vectorSource.addFeature(feature);
+
+      // Create vector layer attached to the vector source.
+      const vectorLayer = new ol.layer.Vector({
+        source: vectorSource
+      });
+
+      $timeout(function(){
+        vm.initMap();
+      }, 100);
+
+      vm.initMap = function(){
+
+        vm.raster = new ol.layer.Tile({
+          source: new ol.source.XYZ({
+            url: 'https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicG9sYW5jb21lbnNhamVyb3MiLCJhIjoiY2oxZTA5eGJwMDAzaDJxa2V2MmhrYmJlaCJ9.dPXLNDhHsxUls7hkNLjTPQ'
+          })
+        });
+
+        vm.source = new ol.source.Vector({wrapX: false});
+
+        vm.vector = new ol.layer.Vector({
+          source: self.source
+        });
+
+        vm.map = new ol.Map({
+          layers: [vm.raster, vm.vector],
+          target: 'map',
+          view: new ol.View({
+            center: [-11000000, 4600000],
+            zoom: 4
+          })
+        });
+
+        // Add the vector layer to the map.
+        vm.map.addLayer(vectorLayer);
+        const location = ol.proj.transform([pointsArray[1][0], pointsArray[1][1]], 'EPSG:4326', 'EPSG:3857');
+        vm.map.getView().setCenter(location);
+        vm.map.getView().setZoom(12)
+      }
+
+      },
       controllerAs: 'vm',
-      template: template,
+      template: viewZoneModal,
       parent: angular.element(document.body),
       targetEvent: ev,
       clickOutsideToClose: true,
       fullscreen: true
     })
-      .then(function (answer) {
-        console.log(answer);
-      }, function () {
-        console.log('Dialog closed');
-      });
-  }
-
-  openMenu(menu, delay = 0) {
-    this.timeout(function () {
-      menu.open();
-    }, delay);
-  }
-
-  openViewZoneModal(ev){
-    this.testingModals(ev, viewZoneModal, viewZoneModalController);
+    .then(function (answer) {
+      console.log(answer);
+    }, function () {
+      console.log('Dialog closed');
+    });
   }
 
 }
